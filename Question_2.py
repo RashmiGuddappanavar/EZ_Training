a851d733ab1e7300083ac271df25fbd265a7a272
RashmiGuddappanavar <140267494+RashmiGuddappanavar@users.noreply.github.com>
2024-06-15 20:50:39 +0530

    Create Question_2.py

---
+++ Question_2.py
@@ -0,0 +1,8 @@
+'''
+Max is having a dog. He want to find the age of the dog he came to know that, the age of the dog is measured with respect to human
+Example: 1 year of life span '''
+
+def calculate_age(n):
+    return n*7
+n=int(input())
+print(calculate_age(n))
