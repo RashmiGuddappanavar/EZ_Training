b3b2fdb062d5b0a51fe18a9c037525c065cd7231
RashmiGuddappanavar <140267494+RashmiGuddappanavar@users.noreply.github.com>
2024-06-15 21:19:31 +0530

    Create Question_5.py

---
+++ Question_5.py
@@ -0,0 +1,7 @@
+'''How To count the spaces in a given statement in Python'''
+
+statement = "This is an example statement."
+
+space_count = statement.count(' ')
+
+print(f"The number of spaces in the statement is: {space_count}")
